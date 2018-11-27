94bf048e5045b8ff6e34782694dbd89f5edbbbc8
Pol Sanz <pol@systemberri.com>
2018-11-27 18:50:44 +0100

    Dashboard UI Started.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<b-container fluid>
+    <b-row>
+        <b-col cols="4">
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+            <b-form class="my-3 mx-2">
+                <b-form-input class="text-center"
+                    type="text" placeholder="Buscar un contacto...">
+                </b-form-input>
+            </b-form>
+
+            <b-list-group>
+                <b-list-group-item variant="dark">
+
+                    <b-row class="p-2" align-h="center">
+                        <b-col cols="12" md="3" class="text-center">
+                            <b-img rounded="circle" blank width="60" height="60" blank-color="#777" alt="img" class="m-1" />
+                        </b-col>
+                        <b-col cols="6" align-self="center" class="d-none d-md-block">
+                            <p class="mb-1">Juan Ramos</p>
+                            <p class="text-muted small mb-1">Tú: ¡Hasta luego lucah!</p>
+                        </b-col>
+                        <b-col cols="3" class="d-none d-md-block">
+                            <p class="text-muted small">1:37 pm</p>                
+                        </b-col>
+                    </b-row>
+                    
+                </b-list-group-item>
+
+                <b-list-group-item>
+                    <b-row class="p-2" align-h="center">
+                        <b-col cols="12" md="3" class="text-center">
+                            <b-img rounded="circle" blank width="60" height="60" blank-color="#777" alt="img" class="m-1" />
+                        </b-col>
+                        <b-col cols="6" align-self="center" class="d-none d-md-block">
+                            <p class="mb-1">Juan Ramos</p>
+                            <p class="text-muted small mb-1">Tú: ¡Hasta luego lucah!</p>
+                        </b-col>
+                        <b-col cols="3" class="d-none d-md-block">
+                            <p class="text-muted small">1:37 pm</p>                
+                        </b-col>
+                    </b-row>
+                </b-list-group-item>
+
+                <b-list-group-item variant="secondary">
+                    <b-row class="p-2" align-h="center">
+                        <b-col cols="12" md="3" class="text-center">
+                            <b-img rounded="circle" blank width="60" height="60" blank-color="#777" alt="img" class="m-1" />
+                        </b-col>
+                        <b-col cols="6" align-self="center" class="d-none d-md-block">
+                            <p class="mb-1">Juan Ramos</p>
+                            <p class="text-muted small mb-1">Tú: ¡Hasta luego lucah!</p>
+                        </b-col>
+                        <b-col cols="3" class="d-none d-md-block">
+                            <p class="text-muted small">1:37 pm</p>                
+                        </b-col>
+                    </b-row>
+                </b-list-group-item>            
+
+            </b-list-group>
+
+        </b-col>
+
+        <b-col cols="8">
+            <b-row>
+                <b-col cols="8"></b-col>
+                <b-col cols="4">
+                    <p>Usuario seleccionado</p>
+                    <hr>
+                    <b-form-checkbox>
+                        Desactivar notificaciones
+                    </b-form-checkbox>
+                </b-col>
+            </b-row>
+        </b-col>
+    </b-row>
+</b-container>
+
 @endsection
